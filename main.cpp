bb1f9c19ef623786073b7dd3dcaeb96de4c0d197
ddoemonn <ozergokalpsezer@gmail.com>
2024-04-07 22:04:17 +0300

    with pointers

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@
 using namespace std;
 
 int main() {
-    bool value = accept3();
-    cout << value << endl;
-    return 0;
+    int v[6] = {1,2,3,4,5,6};
+    int* p = &v[3];
+    cout << p << endl; // 0x16d79708c
+
+    cout << (p+1) << endl; // 0x16d797090
+
+    char c[12] = {'a','b','c','d','e','f','a','b','c','d','e','f'};
+    char* d = &c[0];
+
+    //cout << d << endl; 
+    cout << static_cast<void*>(d) << endl;
+
+    //cout << (d+2) << endl; 
+    cout << static_cast<void*>(d+2) << endl;
+
 }
